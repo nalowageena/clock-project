fcd4f9814d7305b5ddb884e4b271cc8c6361763d
Nalowa Geena <77391048+nalowageena@users.noreply.github.com>
2022-10-01 21:13:21 +0100

    style sidebar

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-const closeSideBtn = document.querySelector(".fa-x-mark");
-const sideBar = document.querySelector("aside");
 
-closeSideBtn.addEventListener('click', closeSideBar)
 
-function closeSideBar() {
-  sideBar.classList.toggle('close')
-}
