5f59ea39bb7b0cdc51449d9141b193bc576cc557
Nalowa Geena <77391048+nalowageena@users.noreply.github.com>
2022-10-27 09:45:42 +0100

    make stopwatch

--- stopwatch/stopwatch.js
+++ stopwatch/stopwatch.js
@@ -1,20 +1,66 @@
-function startStopWatch() {
-    
+let startBtn = document.querySelector("#start");
+let stopBtn = document.querySelector("#stop");
+let resetBtn = document.querySelector("#reset");
+
+let time = document.querySelector(".time");
+let secondDisplay = document.querySelector(".second");
+let minuteDisplay = document.querySelector(".minute");
+let hourDisplay = document.querySelector(".hour");
+
+startBtn.addEventListener("click", startWatch);
+stopBtn.addEventListener("click", stopWatch);
+resetBtn.addEventListener("click", resetWatch);
+
+let second = parseInt(secondDisplay.innerText);
+let minute = parseInt(minuteDisplay.innerText);
+let hour = parseInt(hourDisplay.innerText);
+
+let intervalId;
+
+function startWatch() {
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+  intervalId = setInterval(updateTime, 1000);
 }
 
-function getCurrentTime() {
-    const currentDate = new Date();
-    let hours = currentDate.getHours();
-    let minutes = currentDate.getMinutes();
-    if (minutes / 10 < 1) {
-      minutes = "0" + minutes;
-    }
-    if (hours / 10 < 1) {
-      hours = "0" + hours;
-    }
-    let currentTime = hours + ":" + minutes;
-    console.log(currentTime);
-    checkAlarm(alarmArray, currentTime);
+function stopWatch() {
+  clearInterval(intervalId);
+}
+
+function resetWatch() {
+  clearInterval(intervalId);
+  second = 0;
+  minute = 0;
+  hour = 0;
+  displayTime();
+}
+
+function updateTime() {
+  second++;
+
+  if (second / 60 === 1) {
+    second = 0;
+    minute++;
   }
-  
-  setInterval(getCurrentTime, 1000);
+
+  if (minute / 60 === 1) {
+    minute = 0;
+    hour++;
+  }
+
+  displayTime();
+}
+
+function displayTime() {
+  secondDisplay.innerText = format(second);
+  minuteDisplay.innerText = format(minute);
+  hourDisplay.innerText = format(hour);
+}
+
+function format(num) {
+  if (num < 10) {
+    num = "0" + num;
+  }
+  return num;
+}
