a9fda47cd238d84e460d01a785de043938b9da69
Nalowa Geena <77391048+nalowageena@users.noreply.github.com>
2022-10-30 13:50:50 +0100

    make timer work

---
+++ timer/timer.js
@@ -0,0 +1,90 @@
+let timerForm = document.forms[0];
+let startBtn = document.querySelector("#start");
+let stopBtn = document.querySelector("#stop");
+let resetBtn = document.querySelector("#reset");
+
+let tickSound = new Audio("http://soundbible.com/grab.php?id=2044&type=mp3");
+
+let time = document.querySelector(".time");
+let secondDisplay = document.querySelector(".second");
+let minuteDisplay = document.querySelector(".minute");
+let hourDisplay = document.querySelector(".hour");
+
+startBtn.addEventListener("click", startWatch);
+stopBtn.addEventListener("click", stopWatch);
+resetBtn.addEventListener("click", resetWatch);
+
+let intervalId;
+let second;
+let minute;
+let hour;
+
+function startWatch(event) {
+  event.preventDefault();
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+  second = timerForm.second.value;
+  minute = timerForm.minute.value;
+  hour = timerForm.hour.value;
+  displayTime();
+  intervalId = setInterval(updateTime, 1000);
+}
+
+function stopWatch() {
+  console.log("here");
+  clearInterval(intervalId);
+  stopSound(tickSound);
+}
+
+function resetWatch() {
+  clearInterval(intervalId);
+  second = 0;
+  minute = 0;
+  hour = 0;
+  displayTime();
+  stopSound(tickSound);
+}
+
+function updateTime() {
+  if (second > 0) {
+    second--;
+  }
+
+  if (minute > 0 && second === 0) {
+    second = 59;
+    minute--;
+  }
+
+  if (hour > 0 && minute === 0) {
+    minute = 59;
+    hour--;
+  }
+  playSound(tickSound);
+  displayTime();
+
+  if (second == 0 && minute == 0 && hour == 0) {
+    stopWatch();
+  }
+}
+
+function displayTime() {
+  secondDisplay.innerText = format(second);
+  minuteDisplay.innerText = format(minute);
+  hourDisplay.innerText = format(hour);
+}
+
+function format(num) {
+  if (num < 10) {
+    num = "0" + num;
+  }
+  return num;
+}
+
+function playSound(tickSound) {
+  tickSound.play();
+}
+
+function stopSound(tickSound) {
+  tickSound.pause();
+}
